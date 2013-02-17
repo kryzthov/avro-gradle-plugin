ee1de236ab7ea827c191844d65324db3afff53ca
Christophe Taton <taton@wibidata.com>
2013-02-16 16:01:29 -0800

    WIP

--- src/main/groovy/org/apache/avro/gradle/AvroPlugin.groovy
+++ src/main/groovy/org/apache/avro/gradle/AvroPlugin.groovy
@@ -3,15 +3,18 @@ package org.apache.avro.gradle
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+/**
+ * Plugin to compile Avro definitions to Java generated sources.
+ */
 class AvroPlugin implements Plugin<Project> {
 
-    @Override
-    void apply( final Project project ) {
-
-        project.task( type: AvroCompileTask, 'compileAvro' )
-
-        project.afterEvaluate {
-            project.tasks.compileAvro.execute()
-        }
-    }
+  /** {@inheritDoc} */
+  @Override
+  void apply(final Project project) {
+    project.task(type: AvroCompileTask, 'compileAvro')
+    // TODO: There must be a better way than that:
+    // project.afterEvaluate {
+    //   project.tasks.compileAvro.execute()
+    // }
+  }
 }
